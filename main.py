98145d5a4a9d269792599f6fe9bea15ca5a11c67
lojinmsleh111-stack <lojinmsleh111@gmail.com>
2026-08-28 18:09:24 +0400

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,11 @@ class HealthHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(b"Bot is online!")
 
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+
     def log_message(self, format, *args):
         return
 
